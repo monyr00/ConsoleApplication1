7e626eefc5fcd0ece982e264fcac607e3a632781
منير البرناوي <brnawymnyr@gmail.com>
2026-07-30 22:51:51 +0300

    Added Register Transfer Log

--- ConsoleApplication1/clsTransferScreen.h
+++ ConsoleApplication1/clsTransferScreen.h
@@ -72,7 +72,7 @@ public:
         cin >> Answer;
         if (Answer == 'Y' || Answer == 'y')
         {
-            if (SourceClient.Transfer(Amount, DestinationClient))
+            if (SourceClient.Transfer(Amount, DestinationClient,CurrentUser.UserName))
             {
                 cout << "\nTransfer done successfully\n";
             }
